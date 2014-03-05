ca96c5aa2498e396724030d91f04090cfe595746
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-05 21:01:09 +0100

    Adapt hook

--- init.el
+++ init.el
@@ -16,20 +16,26 @@
 (require 'exec-path-from-shell)
 (exec-path-from-shell-initialize)
 
-;; With this snippet, another press of C-d will kill the buffer.
+;; With this snippet, another press of C-d will kill the term buffer.
 (defun comint-delchar-or-eof-or-kill-buffer (arg)
   (interactive "p")
   (if (null (get-buffer-process (current-buffer)))
       (kill-buffer)
     (comint-delchar-or-maybe-eof arg)))
 
-(defun shell-pack/hook-fn ()
-  "Deactivate smartscan to be able to parse history"
+(defun shell-pack/mode-and-simple-bindings-fn ()
+  "Simple binding definition and add smartscan mode"
   (local-set-key (kbd "C-c C-j") 'term-line-mode)
-  (local-set-key (kbd "C-d") 'comint-delchar-or-eof-or-kill-buffer)
   (smartscan-mode))
 
-(dolist (hook '(sh-mode-hook term-mode-hook shell-mode-hook eshell-mode-hook))
-  (add-hook hook 'shell-pack/hook-fn))
+(dolist (hook '(sh-mode-hook term-mode-hook shell-mode-hook eshell-mode-hook ))
+  (add-hook hook 'shell-pack/mode-and-simple-bindings-fn))
+
+(defun shell-pack/close-buffer-hook-fn ()
+  "Add a binding that permits to kill the buffer when C-d if no input char to kill."
+  (local-set-key (kbd "C-d") 'comint-delchar-or-eof-or-kill-buffer))
+
+(dolist (hook '(term-mode-hook shell-mode-hook eshell-mode-hook ))
+  (add-hook hook 'shell-pack/close-buffer-hook-fn))
 
 ;;; shell-pack.el ends here
