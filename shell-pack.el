d7c8b068369bb1b0beae45e35895e3bb9e6ce102
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-05-28 18:31:46 +0200

    Doc - Update docstring

--- shell-pack.el
+++ shell-pack.el
@@ -24,15 +24,15 @@
 
 (exec-path-from-shell-initialize)
 
-;; With this snippet, another press of C-d will kill the term buffer.
 (defun comint-delchar-or-eof-or-kill-buffer (arg)
+  "Delete ARG char or kill buffer if we hit the end of the file."
   (interactive "p")
   (if (null (get-buffer-process (current-buffer)))
       (kill-buffer)
     (comint-delchar-or-maybe-eof arg)))
 
 (defun shell-pack/mode-and-simple-bindings-fn ()
-  "Simple binding definition and add smartscan mode"
+  "Simple binding definition and add smartscan mode."
   (local-set-key (kbd "C-c C-j") 'term-line-mode)
   (smartscan-mode))
 
@@ -40,10 +40,10 @@
   (add-hook hook 'shell-pack/mode-and-simple-bindings-fn))
 
 (defun shell-pack/close-buffer-hook-fn ()
-  "Add a binding that permits to kill the buffer when C-d if no input char to kill."
+  "Hook function to kill the buffer given a specific binding."
   (local-set-key (kbd "C-d") 'comint-delchar-or-eof-or-kill-buffer))
 
-(dolist (hook '(term-mode-hook shell-mode-hook eshell-mode-hook ))
+(dolist (hook '(term-mode-hook shell-mode-hook eshell-mode-hook))
   (add-hook hook 'shell-pack/close-buffer-hook-fn))
 
 (provide 'shell-pack)
